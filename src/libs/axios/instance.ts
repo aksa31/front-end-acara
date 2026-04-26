54b1f46a7d82f5a2a524a08a78c978d4a76ac25e
aksa31 <147188737+aksa31@users.noreply.github.com>
2026-04-26 18:48:22 +0700

    feat: init project

---
+++ src/libs/axios/instance.ts
@@ -0,0 +1,37 @@
+import environment from "@/config/environment";
+import axios, { AxiosInstance } from "axios";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+
+interface CustomSession extends Session {
+  accessToken?: string;
+}
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+const instance = axios.create({
+  baseURL: environment.API_URL,
+  headers,
+  timeout: 60 * 1000,
+});
+
+instance.interceptors.request.use(
+  async (request) => {
+    const session: CustomSession | null = await getSession();
+    console.log("Session in interceptor:", session);
+    if (session && session.accessToken) {
+      request.headers.Authorization = `Bearer ${session.accessToken}`;
+    }
+    return request;
+  },
+  (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+  (error) => Promise.reject(error),
+);
